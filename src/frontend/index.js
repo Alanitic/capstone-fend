54465d8e94433463885768c093bc5c9ccc7f0ba7
Alan Islas <alan.islas.itic@gmail.com>
2021-08-16 14:32:25 -0500

    Design completed

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,1 +1,23 @@
 import './styles/style.scss';
+
+const LoadCountries = async () => {
+  const request = await fetch(
+    'https://restcountries.eu/rest/v2/all?fields=name;alpha2Code'
+  );
+  try {
+    const allData = await request.json();
+    const combo = document.querySelector('#country');
+    const fragment = document.createDocumentFragment();
+    for (let item of allData) {
+      let option = document.createElement('option');
+      option.textContent = item.name;
+      option.value = item.alpha2Code;
+      fragment.appendChild(option);
+    }
+    combo.appendChild(fragment);
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+LoadCountries();
