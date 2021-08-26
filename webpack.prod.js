c97fe3fde6504634317e4c19bd689711f6758297
Alan Islas <alan.islas.itic@gmail.com>
2021-08-26 18:07:50 -0500

    Service worker added

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
   entry: './src/frontend/index.js',
@@ -27,7 +27,7 @@ module.exports = {
       template: './src/frontend/views/index.html',
       filename: './index.html',
     }),
-    // new WorkboxPlugin.GenerateSW(),
+    new WorkboxPlugin.GenerateSW(),
     new MiniCssExtractPlugin({ filename: '[name].css' }),
   ],
 };
