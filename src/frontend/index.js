ef653057018abc060d86bbb7aa2066b6ef53e7ae
Alan Islas <alan.islas.itic@gmail.com>
2021-08-26 18:01:09 -0500

    Responsive: Extra large design completed

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,7 @@
 import './styles/style.scss';
 import './styles/medium-size.scss';
 import './styles/large-size.scss';
+import './styles/extra-large-size.scss';
 
 import { LoadCountries } from './js/app';
 import { handleSubmit } from './js/form';
