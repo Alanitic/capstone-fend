4e7658a040c9c5119480608fba1cac85dbf772a0
Alan Islas <alan.islas.itic@gmail.com>
2021-08-30 16:54:41 -0500

    Jest test completed on front

--- __test__/frontend/serverRequest.test.js
+++ __test__/frontend/serverRequest.test.js
@@ -1,14 +1,14 @@
-import 'regenerator-runtime/runtime';
-
-import { getRequest, postRequest } from '../../src/frontend/js/serverRequest';
+import { postRequest } from '../../src/frontend/js/serverRequest';
 
 describe('Fetching data to backend', () => {
   test('should return valid coordinates from NY', async () => {
     const country = 'US';
-    const zp = 1019;
-    const result = await getRequest(
+    const zp = 10019;
+    const { lat, lng, placeName } = await postRequest(
       `http://localhost:8081/coordinates?country=${country}&ZP=${zp}`
     );
-    console.log(result);
+    expect(typeof lat).toBe('number');
+    expect(typeof lng).toBe('number');
+    expect(placeName).not.toBeNull();
   });
 });
