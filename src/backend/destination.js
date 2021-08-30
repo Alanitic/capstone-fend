0d03bb9407389bb9424d82888481aa4b93bdb01f
Alan Islas <alan.islas.itic@gmail.com>
2021-08-30 17:16:42 -0500

    Added dotenv and keys changed

--- src/backend/destination.js
+++ src/backend/destination.js
@@ -1,26 +1,20 @@
-const MAX_ROWS = 10;
-const USER_NAME = 'alan_islas_itic';
-const WEATHER_API_KEY = 'a16ab59908364102890e0d53be290aa1';
-const PIXABAY_KEY = '7052805-c3a2de3d1e6a0eeb52e7f68eb';
-const CATEGORY = 'travel';
-
 const apiRequest = require('./apiRequest');
 
 const getLatLon = async (ZP, country) => {
-  const url = `http://api.geonames.org/postalCodeSearchJSON?postalcode=${ZP}&maxRows=${MAX_ROWS}&username=${USER_NAME}&country=${country}`;
+  const url = `http://api.geonames.org/postalCodeSearchJSON?postalcode=${ZP}&maxRows=${process.env.MAX_ROWS}&username=${process.env.USER_NAME}&country=${country}`;
   const { data } = await apiRequest.getRequest(url);
   const { lat, lng, placeName } = data.postalCodes[0];
   return { lat, lng, placeName };
 };
 
 const getWeatherForecast = async (lat, lng) => {
-  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHER_API_KEY}&lat=${lat}&lon=${lng}`;
+  const url = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lng}`;
   const { data } = await apiRequest.getRequest(url);
   return data;
 };
 
 const getDestinationImg = async (place, per_page) => {
-  const url = `https://pixabay.com/api/?key=${PIXABAY_KEY}&q=${place}&per_page=${per_page}&category=${CATEGORY}`;
+  const url = `https://pixabay.com/api/?key=${process.env.PIXABAY_KEY}&q=${place}&per_page=${per_page}&category=${process.env.CATEGORY}`;
   const {
     data: { hits },
   } = await apiRequest.getRequest(url);
